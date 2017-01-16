d2b07c81d840f9379de3d0694c0485a3ae2655cf
Henry Quinn <henryquinniv@gmail.com>
2017-01-16 14:13:11 -0500

    Set up signup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   get 'users/new'
+  resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :uploads, only: [:index, :new, :create, :destroy]
 
--- db/migrate/20170104174216_create_users.rb
+++ db/migrate/20170104174216_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-    drop_table :users
+    # drop_table :users
     create_table :users do |t|
       t.string :first_name
       t.string :last_name

2a7b1d1447c5f350f6ccb834b7fc9174b3a819b8
Henry Quinn <henryquinniv@gmail.com>
2017-01-26 12:39:09 -0500

    Shoring things up for CAD presentation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Webtranet!"
+      flash[:success] = "User created!"
       redirect_to @user
     else
       render 'new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(first_name: "Admin ",
+             last_name: "Admin",
+             email: "admin@admin.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
+
 User.create!(first_name:  "Henry ",
              last_name: "Quinn",
              email: "henryquinniv@gmail.com",
@@ -26,240 +33,216 @@ User.create!(first_name:  "Joann ",
              password:              "password",
              password_confirmation: "password",
              admin: true)
+##
+#30.times do |n|
+#  first_name  = Faker::Name.first_name
+#  last_name = Faker::Name.last_name
+#  email = "example-#{n+1}@railstutorial.org"
+#  password = "password"
+#  User.create!(first_name: first_name + ' ',
+#               last_name: last_name,
+#               email: email,
+#               password:              password,
+#               password_confirmation: password)
+#end
+##
 
-30.times do |n|
-  first_name  = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(first_name: first_name + ' ',
-               last_name: last_name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
-
-Upload.create!(name: "Test Document",
-               category: "Home",
-               subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
-
-Upload.create!(name: "Test Document",
-               category: "Home",
-               subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
-
-Upload.create!(name: "Test Document",
-               category: "Home",
-               subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
-
-Upload.create!(name: "Test Document",
-               category: "Home",
-               subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
-
-Upload.create!(name: "Test Document",
-               category: "Home",
-               subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
-
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Administrative Office Policies",
                category: "HR",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "HRAccess",
                category: "HR",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "HRMIS - Viewing My Data",
                category: "HR",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Telework Policy",
                category: "HR",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Websense Web Security Software Policy",
                category: "HR",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Adobe Acrobat Resource Center",
                category: "IT",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Corrections with Acrobat Writer",
                category: "IT",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "FAS4T Security Manual",
                category: "IT",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Frequently Asked Questions",
                category: "IT",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Inventory Process",
                category: "IT",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Appearance Bond",
                category: "Forms",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Criminal Cover Sheet",
                category: "Forms",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Criminal Forms & Instructions",
                category: "Forms",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Criminal Offense Notice",
                category: "Forms",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Local Prob Form 246B",
                category: "Forms",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Case Assignment Manual",
                category: "Operations",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Direct Assignment List",
                category: "Operations",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Atorney Case Opening FAQ",
                category: "Operations",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Civil Intake",
                category: "Operations",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Town & County List",
                category: "Operations",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "2015 Annual Report",
                category: "Happenings",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Court Committees List",
                category: "Happenings",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Structure Writing Macro Files",
                category: "Happenings",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Intern Policy",
                category: "Happenings",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Basic First Aid / CPR Certified List",
                category: "Happenings",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Judges Bench Photo",
                category: "Court Family",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "2011 Awards Ceremony",
                category: "Court Family",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "2013 Law Day",
                category: "Court Family",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "2009 Clerk's Office Holiday Party",
                category: "Court Family",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Fitness Center Grand Opening",
                category: "Court Family",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "AO National Forms Reference",
                category: "Chambers",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "2017 Attorney Admissions Schedule",
                category: "Chambers",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Lexis",
                category: "Chambers",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Westlaw",
                category: "Chambers",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Circuit Library",
                category: "Chambers",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Called To Serve Video",
                category: "Jury",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Jury Selection Procedures",
                category: "Jury",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Grand Jury Information",
                category: "Jury",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "List Of Jury Clerks",
                category: "Jury",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
 
-Upload.create!(name: "Test Document",
+Upload.create!(name: "Approved Jury Parking Locations",
                category: "Jury",
                subcategory: "",
-               attachment: "/uploads/upload/attachment/46/TEST.pdf")
+               attachment: "")
